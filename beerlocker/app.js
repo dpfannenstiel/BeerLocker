e845c1da8215dece1bca71d03f0c2c7250d460d1
Dustin Pfannenstiel <dustin@nth-estate.com>
2016-11-10 21:34:25 -0600

    added user and login

--- beerlocker/app.js
+++ beerlocker/app.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
-var Beer = require('./models/beer')
+var beerController = require('./controllers/beer')
+var userController = require('./controllers/user')
+var passport = require('passport')
+var authController = require('./controllers/auth')
 
 mongoose.connect('mongodb://localhost:27017/beerlocker')
 
@@ -11,69 +14,24 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+app.use(passport.initialize())
+
 var port = process.env.PORT || 3000
 
 var router = express.Router()
 
-router.get('/', function(req, res) {
-  res.json({message: 'You are running dangerously low on beer!'})
-})
+router.route('/beers')
+  .post(authController.isAuthenticated, beerController.postBeers)
+  .get(authController.isAuthenticated, beerController.getBeers)
 
-var beersRoute = router.route('/beers')
-beersRoute.post(function(req, res) {
-  var beer = new Beer()
-  beer.name = req.body.name
-  beer.type = req.body.type
-  beer.quantity = req.body.quantity
-  beer.save(function(err) {
-    if (err)
-      res.sender(err)
-    res.json({message: 'Beer added to the locker!', data: beer})
-  })
-})
+router.route('/beers/:beer_id')
+  .get(authController.isAuthenticated, beerController.getBeer)
+  .put(authController.isAuthenticated, beerController.putBeer)
+  .delete(authController.isAuthenticated, beerController.deleteBeer)
 
-beersRoute.get(function(req, res) {
-  Beer.find(function(err, beers) {
-    if (err)
-      res.send(err)
-
-    res.json(beers)
-  })
-})
-
-var beerRoute = router.route('/beers/:beer_id')
-beerRoute.get(function(req, res) {
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err)
-      res.send(err)
-
-    res.json(beer)
-  })
-})
-
-beerRoute.put(function(req, res) {
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err)
-      res.send(err)
-
-    beer.quantity = req.body.quantity
-    beer.save(function(err) {
-      if (err)
-        res.send(err)
-
-      res.json(beer)
-    })
-  })
-})
-
-beerRoute.delete(function(req, res) {
-  Beer.findByIdAndRemove(req.params.beer_id, function(err) {
-    if (err)
-      res.send(err)
-
-    res.json({message: 'Beer removed from the locker!'})
-  })
-})
+router.route('/users')
+  .post(userController.postUsers)
+  .get(authController.isAuthenticated, userController.getUsers)
 
 app.use('/api', router)
 
---
+++ beerlocker/controllers/beer.js
@@ -0,0 +1,60 @@
+var Beer = require('../models/beer')
+
+exports.postBeers = function(req, res) {
+  var beer = new Beer()
+
+  beer.name = req.body.name
+  beer.type = req.body.type
+  beer.quantity = req.body.quantity
+  beer.userId = req.user._id
+
+  beer.save(function(err) {
+    if (err)
+      res.send(err)
+
+    res.json({message: 'Beer added to the locker!', data: beer})
+  })
+}
+
+exports.getBeers = function(req, res) {
+  Beer.find({userId: req.user._id}, function(err, beers) {
+    if (err)
+      res.send(err)
+
+    res.json(beers)
+  })
+}
+
+exports.getBeer = function(req, res) {
+  Beer.find({userId: req.user._id, _id: req.params.beer_id}, function(err, beer) {
+    if (err)
+      res.send(err)
+
+    res.json(beer)
+  })
+}
+
+exports.putBeer = function(req, res) {
+  Beer.find({userId: req.user._id, _id: req.params.beer_id}, function(err, beer) {
+    if (err)
+      res.send(err)
+
+    beer.quantity = req.body.quantity
+
+    beer.save(function(err) {
+      if (err)
+        res.send(err)
+
+      res.json(beer)
+    })
+  })
+}
+
+exports.deleteBeer = function(req, res) {
+  Beer.find({userId: req.user._id, _id: req.params.beer_id}, function(err) {
+    if (err)
+      res.send(err)
+
+    res.json({ message: 'Beer removed from teh locker!'})
+  })
+}
